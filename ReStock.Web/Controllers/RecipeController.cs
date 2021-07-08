1164e0f2e09b683d9161c976205083052557e461
Tuan Nam Truong <ttnam1995@gmail.com>
2021-07-08 16:41:05 +0200

    Initial commit

---
+++ ReStock.Web/Controllers/RecipeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ReStock.Web.Models;
+
+namespace ReStock.Web.Controllers
+{
+    public class RecipeController : Controller
+    {
+
+    }
+}
---
+++ ReStock.Web/Controllers/StockController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ReStock.Web.Models;
+
+namespace ReStock.Web.Controllers
+{
+    public class StockController : Controller
+    {
+        private readonly ILogger<StockController> _logger;
+
+        public StockController(ILogger<StockController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
