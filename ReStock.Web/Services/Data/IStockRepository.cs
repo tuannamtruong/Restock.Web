5be5b2c46002c52d0d19ef6a74fb7d85ee5fcf5a
Tuan Nam Truong <ttnam1995@gmail.com>
2021-07-09 16:57:22 +0200

    Add classes for providng data.

---
+++ ReStock.Web/Services/Data/IStockRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ReStock.Web.Services.Data
+{
+    public interface IStockRepository
+    {
+        IEnumerable<string> GetAll();
+    }
+}
---
+++ ReStock.Web/Services/Data/StockRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReStock.Web.Services.Data
+{
+    public class StockRepository : IStockRepository
+    {
+        public IEnumerable<string> GetAll()
+        {
+            return new List<string>()
+            {
+                "Item 1232323", "Item 2"
+            };
+        }
+    }
+}
