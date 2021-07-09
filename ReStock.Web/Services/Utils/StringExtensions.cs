4683da7f40701150d74b6ab9a19a8e48f3862313
Tuan Nam Truong <ttnam1995@gmail.com>
2021-07-09 16:54:58 +0200

    Add Utils.

---
+++ ReStock.Web/Services/Utils/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace ReStock.Web.Services.Utils
+{
+    public static class StringExtensions
+    {
+        public static string ToUpperFirstChar(this string input) =>
+        input switch
+        {
+            null => throw new ArgumentNullException(nameof(input)),
+            "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+            _ => input.First().ToString().ToUpper() + input.Substring(1)
+        };
+
+        public static string ToUpperFirstCharEachLetter(this string input)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToLower());
+        }
+    }
+}
