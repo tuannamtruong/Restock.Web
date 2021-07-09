6a03075e49d743d9284cbebc9bed5559fe50081b
Tuan Nam Truong <ttnam1995@gmail.com>
2021-07-09 16:57:45 +0200

    Show shopping item.

--- ReStock.Web/Controllers/StockController.cs
+++ ReStock.Web/Controllers/StockController.cs
@@ -1,22 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ReStock.Web.Services.Data;
 using ReStock.Web.ViewModels;
 using System.Diagnostics;
+using System.Linq;
 
 namespace ReStock.Web.Controllers
 {
     public class StockController : Controller
     {
         private readonly ILogger<StockController> _logger;
-
-        public StockController(ILogger<StockController> logger)
+        private IStockRepository _stockRepository;
+        public StockController(ILogger<StockController> logger, IStockRepository stockRepository)
         {
             _logger = logger;
+            _stockRepository = stockRepository;
         }
 
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        
         public IActionResult Index()
         {
-            return View();
+            var viewModel = new StockViewModel();
+            viewModel.ShoppingList = _stockRepository.GetAll().ToList();
+            return View(viewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- ReStock.Web/Startup.cs
+++ ReStock.Web/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ReStock.Web.Services.Data;
 
 namespace ReStock.Web
 {
@@ -19,6 +20,7 @@ namespace ReStock.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddTransient<IStockRepository, StockRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
