b30c8f8e70eccd73289aece4de7f5005e00a4f63
Tuan Nam Truong <ttnam1995@gmail.com>
2021-07-12 14:54:53 +0200

    Get data for ViewModel

--- ReStock.Web/Controllers/StockController.cs
+++ ReStock.Web/Controllers/StockController.cs
@@ -4,16 +4,20 @@ using ReStock.Web.Services.Data;
 using ReStock.Web.ViewModels;
 using System.Diagnostics;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace ReStock.Web.Controllers
 {
     public class StockController : Controller
     {
         private readonly ILogger<StockController> _logger;
-        private IStockRepository _stockRepository;
-        public StockController(ILogger<StockController> logger, IStockRepository stockRepository)
+        private readonly IShoppingItemRepository _shoppingItemRepository;
+        private readonly IStockRepository _stockRepository;
+
+        public StockController(ILogger<StockController> logger, IShoppingItemRepository shoppingItemRepository, IStockRepository stockRepository)
         {
             _logger = logger;
+            _shoppingItemRepository = shoppingItemRepository;
             _stockRepository = stockRepository;
         }
 
@@ -21,11 +25,18 @@ namespace ReStock.Web.Controllers
         //{
         //    return View();
         //}
-        
+
         public IActionResult Index()
         {
+            var stockItems = _stockRepository.GetAll().ToList();
+            Dictionary<Models.StockType, List<(string Name, string Amount)>> grouper =
+                stockItems
+                .GroupBy(x => x.StockType)
+                .ToDictionary(x => x.Key, x => x.Select(a => (a.Name, a.Amount)).ToList());
+
             var viewModel = new StockViewModel();
-            viewModel.ShoppingList = _stockRepository.GetAll().ToList();
+            viewModel.ShoppingListItems = _shoppingItemRepository.GetAll().ToList();
+            viewModel.StockItemGroupByType = grouper;
             return View(viewModel);
         }
 
--- ReStock.Web/ViewModels/StockViewModel.cs
+++ ReStock.Web/ViewModels/StockViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReStock.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,9 @@ namespace ReStock.Web.ViewModels
 {
     public class StockViewModel
     {
-        public List<string> ShoppingList { get; set; }
+        public List<string> ShoppingListItems { get; set; }
+        public Dictionary<StockType, List<(string Name, string Amount)>> StockItemGroupByType {get;set;}
+        //public List<StockItem> StockItems { get; set; }
+        //public Dictionary<StockType, >
     }
 }
