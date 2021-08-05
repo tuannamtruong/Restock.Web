2b7b08297183e96a58520583aba929ac10ef47c9
Tuan Nam Truong <ttnam1995@gmail.com>
2021-08-05 11:57:04 +0200

    Add ViewComponent for filtering recipe category

---
+++ ReStock.Tests/Components/NavigationMenuViewComponentTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.Extensions.Logging;
+using Moq;
+using ReStock.Models;
+using ReStock.Web.Components;
+using ReStock.Web.Controllers;
+using ReStock.Web.Services.Data;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+
+namespace ReStock.Tests.Components
+{
+    public class NavigationMenuViewComponentTests
+    {
+        private readonly NavigationMenuViewComponent _viewComponent;
+
+        public NavigationMenuViewComponentTests()
+        {
+            _viewComponent = new NavigationMenuViewComponent();
+        }
+
+
+        [Fact]
+        public void Invoke_ShouldGetCorrectCategories()
+        {
+            string[] results = ((IEnumerable<string>)(_viewComponent.Invoke()
+                as ViewViewComponentResult).ViewData.Model).ToArray();
+             
+        }
+    }
+}
---
+++ ReStock.Web/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using ReStock.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ReStock.Web.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        public NavigationMenuViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            HashSet<string> categories = new HashSet<string>();
+            foreach(var category in Enum.GetValues(typeof(RecipeCategory)))
+            {
+                categories.Add(category.ToString());
+            }
+            return View(categories);
+        }
+    }
+}
