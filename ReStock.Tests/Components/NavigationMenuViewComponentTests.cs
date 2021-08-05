47e891c149e3a25316834537384be2f2be679b97
Tuan Nam Truong <ttnam1995@gmail.com>
2021-08-05 13:32:04 +0200

    Add test for selected category

--- ReStock.Tests/Components/NavigationMenuViewComponentTests.cs
+++ ReStock.Tests/Components/NavigationMenuViewComponentTests.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
-using Microsoft.Extensions.Logging;
-using Moq;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using ReStock.Models;
 using ReStock.Web.Components;
-using ReStock.Web.Controllers;
-using ReStock.Web.Services.Data;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -27,7 +24,26 @@ namespace ReStock.Tests.Components
         {
             string[] results = ((IEnumerable<string>)(_viewComponent.Invoke()
                 as ViewViewComponentResult).ViewData.Model).ToArray();
-             
+            // How to mock new Enum? 
+            // Inject in constructor?
+            // Inject in function?
+            // No need to test?
+        }
+
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            var categoryToSelect = RecipeCategory.Meat.ToString();
+            _viewComponent.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new ViewContext
+                {
+                    RouteData = new Microsoft.AspNetCore.Routing.RouteData()
+                }
+            };
+            _viewComponent.RouteData.Values["category"] = categoryToSelect;
+            string result = (string)(_viewComponent.Invoke() as ViewViewComponentResult).ViewData["SelectedCategory"];
+            Assert.Equal(categoryToSelect, result);
         }
     }
 }
